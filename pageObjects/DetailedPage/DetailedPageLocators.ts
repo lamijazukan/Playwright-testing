60278a30949d701c7a99bea11a39edbc2b13bfc8
LejlaSinanovic <l.sinanovic.l@gmail.com>
2024-01-09 21:50:20 +0100

    Revert ""Init commit""
    This reverts commit e1d21c12cef0151008e8e7a560aac58c324487b4.

--- pageObjects/DetailedPage/DetailedPageLocators.ts
+++
@@ -1,6 +0,0 @@
-export const locators = {
-    time:".showtime-pill",
-    seatPopup:"text=How Many Seats?",
-    selectButton:"text=Select Seats",
-    layoutURL:"https://in.bookmyshow.com/buytickets/dunki-mumbai/movie-mumbai-ET00326964-MT/20240105#!seatlayout"
-}
--- pageObjects/LoginPage/LoginPageLocators.ts
+++
@@ -1,9 +0,0 @@
-export const locators = {
-    signIn: "text=Sign in",
-    poUp: "text=Get Started",
-    google: "text=Continue with Google",
-    userGreeting: "text=Hi, Guest",
-    inputMail: "#identifierId",
-    buttonNext:"text=次",
-    inputPassword: "[type='password']",
-  };
